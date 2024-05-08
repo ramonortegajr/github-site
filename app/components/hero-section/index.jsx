2d3c5fe023b7754db68a255f75f194759cac1d68
ramram <ramonortegajr1997@gmail.com>
2024-05-08 13:39:32 +0800

    changes from the index

--- app/components/hero-section/index.jsx
+++ app/components/hero-section/index.jsx
@@ -97,7 +97,7 @@ function HeroSection({ profile }) {
               <div>
                 <span className="ml-4 lg:ml-8 mr-2 text-white">name:</span>
                 <span className="text-gray-400">{`'`}</span>
-                <span className="text-green-400">{profile.name}</span>
+                <span className="text-green-400">Ram Ortega</span>
                 <span className="text-gray-400">{`',`}</span>
               </div>
               <div>
